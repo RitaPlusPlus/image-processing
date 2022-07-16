db41c3c92ed611863995b1d96fab61dd7b0bbdac
IvonNikolova <ivonnikolova0101@gmail.com>
2022-07-16 12:04:51 +0300

    editing ...

--- image_processing.h
+++ image_processing.h
@@ -12,40 +12,79 @@ class ImageProcessing : public QMainWindow
 {
     Q_OBJECT
 
-public:
-    ImageProcessing(QWidget *parent = nullptr);
-    ~ImageProcessing();
+    public:
+        ImageProcessing(QWidget *parent = nullptr);
+        ~ImageProcessing();
 
-private slots:
+    private slots:
+        //! Click handler on Gaussian filter selected
+        /*!
+             apply Gaussian filter
+        */
+        void on_pushB_Gaussian_clicked();
 
-    void on_pushB_Gaussian_clicked();
+        //! Click handler on Laplacian filter selected
+        /*!
+             apply Laplacian filter
+        */
+        void on_pushB_Laplacian_clicked();
 
-    void on_pushB_Laplacian_clicked();
+        //! Click handler on Mean filter selected
+        /*!
+             apply Mean filter
+        */
+        void on_pushB_Mean_clicked();
 
-    void on_pushB_Upload_clicked();
+        //! Click handler on Gradient filter selected
+        /*!
+             apply Gradient filter
+        */
+        void on_pushB_Gradient_clicked();
 
-    void on_pushB_Download_clicked();
+        //! Click handler on horizontal Sobel filter selected
+        /*!
+             apply horizontal Sobel filter
+        */
+        void on_pushB_hSobel_clicked();
 
-    void on_pushB_deleteImage_clicked();
+        //! Click handler on vertical Sobel filter selected
+        /*!
+             apply vertical Sobel filter
+        */
+        void on_pushB_vSobel_clicked();
 
-    void on_pushB_Mean_clicked();
+        //! Click handler on Convert an image to Greyscale
+        /*!
+            set the image to be black-and-white coloured
+        */
+        void on_pushB_convertGS_clicked();
 
-    void on_pushB_Gradient_clicked();
+        //! Click handler on Upload clicked
+        /*!
+             upload an image
+        */
+        void on_pushB_Upload_clicked();
 
-    void on_pushB_convertGS_clicked();
+        //! Click handler on Download clicked
+        /*!
+             download the image
+        */
+        void on_pushB_Download_clicked();
 
-    void on_pushB_hSobel_clicked();
+        //! Click handler on Delete clicked
+        /*!
+             delete the image
+        */
+        void on_pushB_deleteImage_clicked();
 
-    void on_pushB_vSobel_clicked();
+    private:
+        Ui::Image_Processing *ui;
 
-private:
-    Ui::Image_Processing *ui;
-
-    QImage sourceImage;
-    /*
-     * QImage is designed and optimized for I/O,
-     * and for direct pixel access and manipulation,
-     * while QPixmap is designed and optimized for showing images on screen.
-    */
+        QImage sourceImage;
+        /*
+         * QImage is designed and optimized for I/O,
+         * and for direct pixel access and manipulation,
+         * while QPixmap is designed and optimized for showing images on screen.
+        */
 };
 #endif // IMAGE_PROCESSING_H
